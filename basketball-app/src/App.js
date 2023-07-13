d58d0ad77515c9319a7e1cbb323be0779953feee
Harry Marshall <harrymarshall@protonmail.com>
2023-07-13 15:36:44 +1000

    react first commit

---
+++ basketball-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <title>Basketball App</title>
+      </header>
+
+      <body> 
+        <h1>Basketball</h1>
+        <p>Hello</p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
