6c79a9db77446f309a0aa01ff086f3125e0de427
Harry Marshall <harryyymarshall@gmail.com>
2023-11-08 00:57:43 +1100

    svg logo delete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    
+        <section class="hero">
+            <h1>Welcome to My Website</h1>
+            <p>Your go-to place for amazing content.</p>
+            <a href="#" class="cta-button">Get Started</a>
+        </section>
+    
+        <footer>
+            <p>&copy; 2023 My Website. All rights reserved.</p>
+        </footer>
+    </body>
     </div>
   );
 }
